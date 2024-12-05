5bd7ab4d4d76a6b8ab4cf7282a7b6d68feb77aa2
aaguy-hue <aaditmathur6@gmail.com>
2024-12-04 18:06:08 -0800

    kill program if no bot token

--- src/main.go
+++ src/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"github.com/shuban-789/bjorn/src/bot"
-	"os"
 	"fmt"
+	"os"
+
+	"github.com/shuban-789/bjorn/src/bot"
 )
 
 func main() {
 	BotToken := os.Getenv("DSC_BOT_TOKEN")
 	if BotToken == "" {
 		fmt.Println("\033[31m[FAIL]\033[0m No Bot Token found")
+		return
 	}
 	bot.Deploy(BotToken)
 }
