3797307a0d360e16b5f3a86f3378f6c36080e907
shuban-789 <cybergeek11929@gmail.com>
2024-12-05 20:14:37 -0800

    help menu is fixed

--- src/bot/help.go
+++ src/bot/help.go
@@ -14,11 +14,15 @@ func helpcmd(ChannelID string, session *discordgo.Session) {
 			},
 			&discordgo.MessageEmbedField{
 				Name:  ">>ping",
-				Value: "Bot response latency",
+				Value: "Get bot response latency",
 			},
 			&discordgo.MessageEmbedField{
-				Name:  ">>team [team_id]",
+				Name:  ">>team [team_id] [optional: stats, awards]",
 				Value: "Return information about a team",
+			},
+			&discordgo.MessageEmbedField{
+				Name:  ">>roleme [team_id]",
+				Value: "Assign yourself a role based on your team\nnumber (San Diego FTC teams only)",
 			},
 		},
 	}
