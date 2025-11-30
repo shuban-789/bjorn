69701d74f761eea22e1c00c5bf63a0551387badc
aaguy-hue <aaditmathur6@gmail.com>
2025-11-29 22:54:28 -0800

    remove redundant newlines in logs

--- src/bot/log.go
+++ src/bot/log.go
@@ -3,13 +3,13 @@ package bot
 import "fmt"
 
 func success(msg string, args ...any) string {
-	return fmt.Sprintf("\033[32m[SUCCESS]\033[0m "+msg+"\n", args...)
+	return fmt.Sprintf("\033[32m[SUCCESS]\033[0m "+msg, args...)
 }
 
 func fail(msg string, args ...any) string {
-	return fmt.Sprintf("\033[31m[FAIL]\033[0m "+msg+"\n", args...)
+	return fmt.Sprintf("\033[31m[FAIL]\033[0m "+msg, args...)
 }
 
 func info(msg string, args ...any) string {
-	return fmt.Sprintf("\033[33m[INFO]\033[0m "+msg+"\n", args...)
+	return fmt.Sprintf("\033[33m[INFO]\033[0m "+msg, args...)
 }
